8ef73be92f3458e268e3408d731995e66bb15dd0
xalberteinsteinx <xalbert.einsteinx@gmail.com>
2016-10-31 10:27:00 +0200

    Init.

---
+++ backend/Module.php
@@ -0,0 +1,16 @@
+<?php
+namespace bl\cms\payment\backend;
+
+/**
+ * @author Albert Gainutdinov <xalbert.einsteinx@gmail.com>
+ */
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'bl\cms\payment\backend\controllers';
+
+    public function init()
+    {
+        parent::init();
+    }
+
+}
---
+++ frontend/Module.php
@@ -0,0 +1,16 @@
+<?php
+namespace bl\cms\payment\frontend;
+
+/**
+ * @author Albert Gainutdinov <xalbert.einsteinx@gmail.com>
+ */
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'bl\cms\payment\frontend\controllers';
+
+    public function init()
+    {
+        parent::init();
+    }
+
+}
